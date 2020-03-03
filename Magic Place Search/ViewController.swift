6ff98271f2337575d071e735d33580e660422b49
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-03-03 15:52:34 +0000

    Initial Commit

---
+++ Magic Place Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic Place Search
+//
+//  Created by Sean Williams on 03/03/2020.
+//  Copyright © 2020 Sean Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
